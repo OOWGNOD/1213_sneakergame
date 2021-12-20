9a679b3653643b60db7188ffa307d12346403124
Dongwoo Yu <dongwoozzik@naver.com>
2021-12-20 14:24:40 +0900

    1213.sneakergame kakaoapi
    kakaoapi를 통한 공유하기 버튼 추가

---
+++ js/share.js
@@ -0,0 +1,34 @@
+const url = 'https://oowgnod.github.io';
+
+function setShare(){
+    let resultImg = document.querySelector('.result-img');
+    let resultAlt = resultImg.firstElementChild.alt;
+    const shareTitle = '스니커즈 게임 결과';
+    const shareDes = infoList[resultAlt].name;
+    const shareImg = url + 'img/image-' + resultAlt + '.jpg';
+    const shareURL = url + 'page/result-' + resultAlt + '.html';
+
+        Kakao.Link.sendDefault({
+                objectType: 'feed',
+                content: {
+                title: shareTitle,
+                description: shareDes,
+                imageUrl: shareImg,
+                link: {
+                    mobileWebUrl: shareURL,
+                    webUrl: shareURL,
+                },
+                },
+                buttons: [
+                {
+                    title: '스니커즈 게임 결과 확인',
+                    link: {
+                    mobileWebUrl: shareURL,
+                    webUrl : shareURL
+                    },
+                },
+                ]
+            });
+        }
+
+
